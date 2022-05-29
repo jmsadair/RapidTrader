3b8e9389e669b5f9a0b5de8c9c329510e2523f10
jmsadair <jmsadair@gmail.com>
2022-05-29 17:08:29 -0400

    Wrote initial tests for sending/receiving messages

---
+++ test/messaging_test.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include <future>
+#include <thread>
+#include "receiver.h"
+
+// Messages for testing.
+struct AddMessage {};
+struct SubtractMessage {};
+
+struct MessagingTester {
+    Messaging::Receiver receiver;
+    int num = 0;
+    void start() {
+        try {
+            while (true) {
+                receiver.wait()
+                        .handle<AddMessage>([&](const AddMessage &msg) { ++num; })
+                        .handle<SubtractMessage>([&](const SubtractMessage &msg) { --num; });
+            }
+        } catch(const Messaging::CloseQueue&) {}
+    }
+};
+
+TEST(MessagingTest, SenderAndRecieverShouldWork) {
+    MessagingTester tester;
+    auto sender = static_cast<Messaging::Sender>(tester.receiver);
+    std::thread t1 {&MessagingTester::start, &tester};
+    // Initialize some messages for testing.
+    AddMessage test_msg1;
+    SubtractMessage test_msg2;
+    // Expected value after sending the first message.
+    int expected_num1 = 2;
+    // Add 3 to num.
+    sender.send(test_msg1);
+    sender.send(test_msg1);
+    sender.send(test_msg1);
+    // Subtract 1 from num.
+    sender.send(test_msg2);
+    sender.send(Messaging::CloseQueue());
+    t1.join();
+    ASSERT_EQ(tester.num, expected_num1);
+}
