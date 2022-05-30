d6a691e5ed3d90b4429176f115cbe1377f4f36fa
jmsadair <jmsadair@gmail.com>
2022-05-30 12:31:31 -0400

    Updated exchange message types

--- src/messages/messages.h
+++ src/messages/messages.h
@@ -1,6 +1,28 @@
 #ifndef FAST_EXCHANGE_MESSAGES_H
 #define FAST_EXCHANGE_MESSAGES_H
-#include <cstdint>
-struct PlaceOrder {};
-struct CancelOrder {};
+#include "order.h"
+struct PlaceOrder {
+    UserID user_id;
+    OrderID order_id;
+    OrderAction order_action;
+    OrderSide order_side;
+    OrderType order_type;
+};
+
+struct CancelOrder {
+    UserID user_id;
+    OrderID order_id;
+};
+
+struct OrderExecuted {
+    UserID user_id;
+    OrderID order_id;
+};
+
+struct OrderCancelled {
+    UserID user_id;
+    OrderID order_id;
+    Quantity remaining_quantity;
+    OrderStatus order_status;
+};
 #endif //FAST_EXCHANGE_MESSAGES_H
