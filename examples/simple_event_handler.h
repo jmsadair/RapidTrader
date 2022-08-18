3eb6e1677707383f6123585c7cafe4f1c9c97f0d
James <jmsadair@gmail.com>
2022-08-18 17:53:46 -0400

    Cleaned up simple event handler

--- examples/simple_event_handler.h
+++ examples/simple_event_handler.h
@@ -11,32 +11,26 @@ protected:
     void handleOrderAdded(const OrderAdded &notification) override
     {
         std::cout << notification << std::endl;
-        ;
     }
     void handleOrderDeleted(const OrderDeleted &notification) override
     {
         std::cout << notification << std::endl;
-        ;
     }
     void handleOrderUpdated(const OrderUpdated &notification) override
     {
         std::cout << notification << std::endl;
-        ;
     }
     void handleOrderExecuted(const ExecutedOrder &notification) override
     {
         std::cout << notification << std::endl;
-        ;
     }
     void handleSymbolAdded(const SymbolAdded &notification) override
     {
         std::cout << notification << std::endl;
-        ;
     }
     void handleSymbolDeleted(const SymbolDeleted &notification) override
     {
         std::cout << notification << std::endl;
-        ;
     }
 };
 #endif // RAPID_TRADER_SIMPLE_EVENT_HANDLER_H
